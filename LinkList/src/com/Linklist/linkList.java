34b4e2855356f3402e69ae8c0b028964fb1727be
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-16 01:17:33 +0530

    Create simple Link list

--- LinkList/src/com/Linklist/linkList.java
+++ LinkList/src/com/Linklist/linkList.java
@@ -1,9 +1,19 @@
 package com.Linklist;
 
+import java.util.LinkedList;
+
 public class linkList {
 
 	public static void main(String[] args) {
-		System.out.println("welcome to the link list problem");
+		// Creating object of the
+		// class linked list
+		LinkedList<Integer> ll = new LinkedList<Integer>();
+
+		// Adding elements to the linked list
+		ll.add(56);
+		ll.add(30);
+		ll.add(70);
+		System.out.println(ll);
 
 	}
 
