426b27711296737e7380b8cd44194710a09f70f4
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-16 02:44:10 +0530

    insert Element between given in the linklist

--- LinkList/src/com/Linklist/MainList.java
+++ LinkList/src/com/Linklist/MainList.java
@@ -1,15 +1,44 @@
 package com.Linklist;
 
-import java.util.LinkedList;
+import java.util.*;
 
 public class MainList {
-	//main method
-	public static void main(String[] args) {	
-		createLinkList linkedList=new createLinkList();
-		linkedList.append(70);
-		linkedList.append(30);
-		linkedList.append(56);
-		System.out.println("LinkList:");
-		linkedList.print();	
+
+	public static void main(String[] args) {
+		// main method
+		LinkList linkedList = new LinkList();
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+
+			System.out.println("Enter your choice\n1)Insert at first (push)\n" + "2)insert at particular index\n" + "3) Exit \n");
+			int choice = sc.nextInt();
+
+			switch (choice) {
+
+			case 1:
+				System.out.println("enter data to insert(push)");
+				int insertlast = sc.nextInt();
+				linkedList.push(insertlast);
+				linkedList.print();
+				break;
+
+			case 2:
+				System.out.println("enter data to insert");
+				int insertBetween = sc.nextInt();
+				System.out.println("enter the index after which you have to enter data");
+				int position = sc.nextInt();
+				linkedList.addAtIndex(insertBetween, position);
+				linkedList.print();
+				break;
+			case 3:
+				System.exit(0);
+				break;
+			default:
+				System.out.println("Invalid choice");
+				break;
+			}
+
+		}
+
 	}
 }
